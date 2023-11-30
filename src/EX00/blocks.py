249d02eb20a68398ec6cf2366c9ae6e361bf17b4
001 <a001@Mac-mini-001.local>
2023-11-30 17:32:17 +0300

    not pep-style

---
+++ src/EX00/blocks.py
@@ -0,0 +1,29 @@
+import sys
+
+def valid():
+	if len(sys.argv) == 2 and sys.argv[1].isdigit():
+		return 1
+	else:
+		return None
+
+def valid_block(string):
+	if len(string) == 32 and string[5] != "0" and string.startswith("00000"):
+			return(string)
+
+def main():
+	if valid():
+		num_lines = int(sys.argv[1])
+		if num_lines <= 10:
+			input_strings = [next(sys.stdin).strip() for _ in range(num_lines)]
+			
+			for string in input_strings:
+				block = valid_block(string)
+				if block is not None:
+					print(block)
+	else:
+		print("ERROR: Wrong arguments!!! \nUsage:")			
+		print("cat data_hashes_10lines.txt | python blocks.py <lines_number>")
+
+if __name__ == "__main__":
+	main()
+	
