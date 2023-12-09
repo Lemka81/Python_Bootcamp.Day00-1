0559a8229455257c1534a8041069778fcad36eff
001 <a001@Mac-mini-001.local>
2023-12-09 13:54:31 +0300

    optim ex01

--- src/EX01/decypher.py
+++ src/EX01/decypher.py
@@ -6,21 +6,11 @@ def valid():
 	else:
 		return None
 
-def make_abbreviation():
-    splitted_string = sys.argv[1].split()
-    abbreviation = ""
-    for word in splitted_string:
-        abbreviation += word[0]
-    return (abbreviation)
-
-
-def main():
-    if valid():
-        code_word = make_abbreviation()
-        print(code_word)
-    else:
-        print("ERROR: Wrong arguments!!! \nUsage:")
-        print("python decypher.py \"Some random words\"")
-
-if __name__ == "__main__":
-    main()
+if valid():
+    line = sys.argv[1].split()
+    for item in line:
+        print(item[0], end='')
+    print()
+else:
+    print("ERROR: Wrong arguments!!! \nUsage:")
+    print("python decypher.py \"Some random text\"")
