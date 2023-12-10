5d075960bbd0b2c0268a14f10c7fae7a1a0a3eec
001 <a001@Mac-mini-001.local>
2023-12-10 23:49:38 +0300

    ex02 done

--- src/EX02/mfinder.py
+++ src/EX02/mfinder.py
@@ -1,34 +1,24 @@
 import sys
 
-def valid_0(line):
-    return line.startswith("*") and line.endswith("*") and\
-            line[1] != "*" and line[2] != "*" and line[3] != "*"
+def check_m(line, i) -> bool:
+    if i == 1:
+        return line[::4] == "**" and "*" not in line[1:4]
+    if i == 2:
+        return line[::3] == line[3:] == "**" and "*" not in line[2]
+    if i == 3:
+        return line[::2] == "***" and "*" not in line[1::2]
 
-def valid_1(line):
-    return line.startswith("**") and line.endswith("**") and\
-            line[2] != "*"
-
-def valid_2(line):
-    return line.startswith("*") and line.endswith("*") and\
-            line[1] != "*" and line[2] == "*" and line[3] != "*"
-def main():
-    m_flag = True
-    for i in range(3):
-        line = next(sys.stdin).strip()
-        m_flag &= len(line) == 5
-        
-        if not m_flag:
+i: int = 0
+for line in sys.stdin:
+    line: str = line.rstrip()
+    if len(line):
+        i += 1
+        if i > 3 or len(line) != 5:
+            print("Error")
             break
-        
-        if m_flag and i == 0:
-            m_flag &= valid_0(line)
-        elif m_flag and i == 1:
-            m_flag &= valid_1(line)
-        elif m_flag and i == 2:
-            m_flag &= valid_2(line)
-        
-    print(m_flag)
-
-
-if __name__ == "__main__":
-    main()
+        else:
+            res: bool = check_m(line, i)
+            if res == 0 or i == 3:
+                print (res)
+                break
+            
