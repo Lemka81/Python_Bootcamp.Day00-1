003b6f9cb24357c90d3e73193ca0496d99e128e7
Lemka Evloeva <cojacque@il-i1.msk.21-school.ru>
2023-12-20 16:15:15 +0300

    ex00 checked

--- src/EX01/decypher.py
+++ src/EX01/decypher.py
@@ -1,16 +1,15 @@
 import sys
 
-def valid():
-	if len(sys.argv) == 2:
-		return 1
-	else:
-		return None
 
-if valid():
-    line: list[str] = sys.argv[1].split()
-    for item in line:
-        print(item[0], end='')
-    print()
-else:
-    print("ERROR: Wrong arguments!!! \nUsage:")
-    print("python decypher.py \"Some random text\"")
+def decypher() -> None:
+    if len(sys.argv) == 2:
+        line: list[str] = sys.argv[1].split()
+        for item in line:
+            print(item[0], end='')
+        print()
+    else:
+        print("ERROR: Wrong arguments!!! \nUsage:")
+        print("python decypher.py \"Some random text\"")
+
+
+decypher()
