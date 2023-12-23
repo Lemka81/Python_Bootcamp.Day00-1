ba218d13c99c97dfcfcd1a2fcb1c8572bbcf5963
001 <a001@Mac-mini-001.local>
2023-12-23 19:39:28 +0300

    ex00

--- src/EX00/blocks.py
+++ src/EX00/blocks.py
@@ -1,24 +1,18 @@
 import sys
 
-def valid():
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        return 1
-    else:
-        return None
 
-def valid_block(string):
-    if len(string) == 32 and string[5] != "0" and string.startswith("00000"):
-        return(string)
+def error_msg() -> None:
+    print("ERROR.\nUsage: cat data_hashes.txt| python blocks.py 10")
 
-if valid():
-    num_lines = int(sys.argv[1])
-    if num_lines <= 10:
-        input_strings = [next(sys.stdin).strip() for _ in range(num_lines)]
-        
-        for string in input_strings:
-            block: str = valid_block(string)
-            if block is not None:
-                print(block)
+
+if len(sys.argv) != 2:
+    error_msg()
 else:
-    print("ERROR: Wrong arguments!!! \nUsage:")			
-    print("cat data_hashes_10lines.txt | python blocks.py <lines_number>")
+    blocks_nbr: str = sys.argv[1]
+    if blocks_nbr.isdigit() and 0 <= int(blocks_nbr) <= 10:
+        for _ in range(int(blocks_nbr)):
+            block: str = input()
+            if len(block) == 32 and block.startswith("00000") and block[5] != "0":
+                print(block)
+    else:
+        error_msg()
