d2f255d59b9811e378291eabba779661076271a1
cojacque <cojacque@student.21-school.ru>
2023-12-23 14:50:48 +0300

    EX01 done

---
+++ EX01/decypher.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def error_msg() -> None:
+    print('ERROR. \nUsage: python decypher.py "Some random text"')
+
+
+def get_decypher():
+    cypher: list[str] = sys.argv[1].split()
+    for word in cypher:
+        print(word[0], end="")
+
+
+if len(sys.argv) < 2:
+    error_msg()
+else:
+    get_decypher()
