9fbfd747094a364cec7798d2ff85f35126d47a38
001 <a001@Mac-mini-001.local>
2023-12-26 03:01:19 +0300

    ex01

--- src/EX01/decypher.py
+++ src/EX01/decypher.py
@@ -1,15 +1,18 @@
 import sys
 
 
-def decypher() -> None:
-    if len(sys.argv) == 2:
-        line: list[str] = sys.argv[1].split()
-        for item in line:
-            print(item[0], end='')
-        print()
-    else:
-        print("ERROR: Wrong arguments!!! \nUsage:")
-        print("python decypher.py \"Some random text\"")
+def error_msg() -> None:
+    print('ERROR. \nUsage: python decypher.py "Some random text"')
 
 
-decypher()
+def get_decypher():
+    cypher: list[str] = sys.argv[1].split()
+    for word in cypher:
+        print(word[0], end="")
+    print()
+
+
+if len(sys.argv) < 2:
+    error_msg()
+else:
+    get_decypher()
